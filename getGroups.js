608fe995fa21ba4384e804757b964456c75a8ec5
Zachary Smith <william.smith1@wipro.com>
2017-02-03 18:42:26 -0500

    Get users and all their groups

--- getGroups.js
+++ getGroups.js
@@ -1,16 +1,23 @@
 const request = require('superagent')
-
 require('dotenv').config()
-
 const baseUri = process.env.DISCOURSE_BASE_URI
-const userEndpoint = `/users/zac.json?api_key=${process.env.DISCOURSE_API_KEY}&amp;api_username=${process.env.DISCOURSE_USER_NAME}`
-const userUri = baseUri + userEndpoint
 
-request
-  .get(userUri)
-  .then(response => {
-    const groups = response.body.user.groups
-    return groups.map(group => group.name)
-  })
-  .then(groups => console.log(groups))
-  .catch(err => console.log(err))
+const getGroups = user => {
+  const userEndpoint = `/users/${user.username}.json?api_key=${process.env.DISCOURSE_API_KEY}&amp;api_username=${process.env.DISCOURSE_USER_NAME}`
+  const userUri = baseUri + userEndpoint
+
+  return request
+    .get(userUri)
+    .then(response => {
+      const groups = response.body.user.groups
+      return groups.map(group => group.name)
+    })
+    .then(groups => ({
+      userId: user.id,
+      username: user.username,
+      groups
+    }))
+    .catch(err => console.log(err))
+}
+
+module.exports = getGroups
---
+++ getUsers.js
@@ -0,0 +1,19 @@
+const request = require('superagent')
+require('dotenv').config()
+
+const getGroups = require('./getGroups')
+
+const baseUri = process.env.DISCOURSE_BASE_URI
+const usersEndpoint = `/admin/users/list/active.json?api_key=${process.env.DISCOURSE_API_KEY}&amp;api_username=${process.env.DISCOURSE_USER_NAME}`
+const usersUri = baseUri + usersEndpoint
+
+request
+  .get(usersUri)
+  .then(response => {
+    const users = response.body
+    return users
+  })
+  .then(users => {
+    Promise.all(users.map(getGroups))
+      .then(result => console.log(result))
+  })
