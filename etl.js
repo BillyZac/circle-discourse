d9843f5e2f21a8c70f8fa0f75a6a88b28a0647d4
Zachary Smith <william.smith1@wipro.com>
2017-02-03 17:37:18 -0500

    Demonstrate GET users in a particular group

--- etl.js
+++ etl.js
@@ -7,33 +7,26 @@ db.defaults({ users: [] })
   .value()
 
 const baseUri = process.env.DISCOURSE_BASE_URI
-
 const usersEndpoint = `/admin/users/list/active.json?api_key=${process.env.DISCOURSE_API_KEY}&amp;api_username=${process.env.DISCOURSE_USER_NAME}`
-unirest('GET', baseUri + usersEndpoint, function(result, error) {
-  if (error) throw error
+const usersUri = baseUri + usersEndpoint
 
-  result.body.forEach(user => {
-    db.get('users')
-    .push({
-      discourseId: user.id,
-      username: user.username,
-      email: user.email,
-      admin: user.admin,
-    })
-      .value()
-  })
-})
-
-// const groupsEndpoint = `/admin/users/list/active.json?api_key=${process.env.DISCOURSE_API_KEY}&amp;api_username=${process.env.DISCOURSE_USER_NAME}`
-// unirest('GET', baseUri + groupsEndpoint, function(result) {
-//   result.body.forEach(user => {
+// unirest('GET', usersUri, function(result) {
+//   console.log(JSON.stringify(result));
+//
+//   const users = result.body
+//   users.forEach(user => {
 //     db.get('users')
 //     .push({
 //       discourseId: user.id,
 //       username: user.username,
+//       email: user.email,
 //       admin: user.admin,
 //     })
 //       .value()
 //   })
 // })
-//
+
+const groupsEndpoint = `/groups/admins/members.json?api_key=${process.env.DISCOURSE_API_KEY}&amp;api_username=${process.env.DISCOURSE_USER_NAME}`
+unirest('GET', baseUri + groupsEndpoint, function(result) {
+  console.log(result.body);
+})
